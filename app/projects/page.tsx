f9f90a3689d32fc98747099657684a5a011f2b96
Reda <wayzfut@gmail.com>
2025-03-25 21:40:49 -0400

    fixed link

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -21,7 +21,7 @@ export default function Projects() {
           <Link href="/projects" className="hover:underline">
             Projects
           </Link>
-          <Link href="https://drive.google.com/file/d/1ye4Kd3ucgdLHQ1uqUi-t5xGb1IOXuOMI/view?usp=sharing" className="hover:underline">
+          <Link href="https://drive.google.com/file/d/1JvJn5QFD4ZESB7uBw3Sus51JHwqkhKnl/view?usp=sharing" className="hover:underline">
             Resume
           </Link>
         </nav>
